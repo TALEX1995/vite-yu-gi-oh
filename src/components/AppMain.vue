ee0b8627a6272a90d281d3589864dc88d3b01822
TALEX1995 <tanase.alex@libero.it>
2023-07-03 16:58:45 +0200

    Added card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -14,5 +14,12 @@ export default {
 </script>
 
 <template>
-    <PokemonCard />
+    <div class="row row-cols-2">
+        <div v-for="{ imageUrl, name, type1, type2, color, generation, hp, atk, def } in store.pokemons" class="col d-flex">
+            <PokemonCard :pokemonImg="imageUrl" :name="name" :type1="type1" :type2="type2" :color="color"
+                :generation="generation" :hp="hp" :atk="atk" :def="def" />
+        </div>
+    </div>
 </template>
+
+
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,6 +1,38 @@
 <script>
+export default {
+    props: {
+        pokemonImg: String,
+        name: String,
+        type1: String,
+        type2: String,
+        color: String,
+        generation: String,
+        hp: Number,
+        atk: Number,
+        def: Number
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <div class="mt-3">
+        <img :src="pokemonImg" alt="name">
+    </div>
+    <div class="d-flex flex-column ms-5 justify-content-center">
+        <span>Nome Pokemon: {{ name }}</span>
+        <span>Tipo 1: {{ type1 }}</span>
+        <span>Tipo 2: {{ type2 }}</span>
+        <span>Colore: {{ color }}</span>
+        <span>Generazione: {{ generation }}</span>
+        <span>HP: {{ hp }}</span>
+        <span>Attacco: {{ atk }}</span>
+        <span>Difesa: {{ def }}</span>
+    </div>
+</template>
 
-<style></style>
+<style scoped>
+img {
+    max-width: 350px;
+    height: 350px;
+}
+</style>
