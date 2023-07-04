800823dbda7de1529a8da207de591676707e61e0
TALEX1995 <tanase.alex@libero.it>
2023-07-04 15:24:13 +0200

    Added select

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,42 @@
 <script>
 import { store } from '../data/store.js';
 import PokemonCard from './PokemonCard.vue';
+import FilterSelect from './FilterSelect.vue';
 
 export default {
     data() {
         return {
-            store
+            store,
+            pokemonType: [
+                "Bug",
+                "Dark",
+                "Dragon",
+                "Electric",
+                "Fairy",
+                "Fighting",
+                "Fire",
+                "Flying",
+                "Ghost",
+                "Grass",
+                "Ground",
+                "Ice",
+                "Normal",
+                "Poison",
+                "Psychic",
+                "Rock",
+                "Steel",
+                "Water"
+            ]
         }
     },
 
-    components: { PokemonCard }
+    components: { PokemonCard, FilterSelect }
 }
 </script>
 
 <template>
+    <h3 class="m-3">Sceglio il tipo di pokemon:</h3>
+    <FilterSelect :filtered-array="pokemonType" />
     <div v-if="store.isLoading" class="fs-1">LOADING ...</div>
     <div v-else class="row row-cols-2">
         <div v-for="{ imageUrl, name, type1, type2, color, generation, hp, atk, def } in store.pokemons" class="col d-flex">
---
+++ src/components/FilterSelect.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+    props: {
+        filteredArray: Array
+    }
+}
+</script>
+
+<template>
+    <select class="form-select w-50 m-3" aria-label="Default select example">
+        <option selected>Nessuno</option>
+        <option v-for="option in filteredArray" value="option">{{ option }}</option>
+    </select>
+</template>
